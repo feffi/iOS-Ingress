1343c000ce383b4e547064a4d0946233d43ff00a
Alex Studnička <astudnicka@gmail.com>
2013-05-30 16:41:38 +0200

    Better portal overlay drawing

--- Ingress/PortalOverlayView.m
+++ Ingress/PortalOverlayView.m
@@ -12,57 +12,29 @@
 
 - (void)drawMapRect:(MKMapRect)mapRect zoomScale:(MKZoomScale)zoomScale inContext:(CGContextRef)context {
 
-//	double maxx = MKMapRectGetMaxX(mapRect);
-//    double minx = MKMapRectGetMinX(mapRect);
-//    double maxy = MKMapRectGetMaxY(mapRect);
-//    double miny = MKMapRectGetMinY(mapRect);
-//	
-//    CGPoint tr = [self pointForMapPoint:(MKMapPoint) {maxx, maxy}];
-//    CGPoint br = [self pointForMapPoint:(MKMapPoint) {maxx, miny}];
-//    CGPoint bl = [self pointForMapPoint:(MKMapPoint) {minx, miny}];
-//    CGPoint tl = [self pointForMapPoint:(MKMapPoint) {minx, maxy}];
-//	
-//    CGMutablePathRef cgPath = CGPathCreateMutable();
-//    CGPathMoveToPoint(cgPath, NULL, tr.x, tr.y);
-//    CGPathAddLineToPoint(cgPath, NULL, br.x, br.y);
-//    CGPathAddLineToPoint(cgPath, NULL, bl.x, bl.y);
-//    CGPathAddLineToPoint(cgPath, NULL, tl.x, tl.y);
-//    CGPathAddLineToPoint(cgPath, NULL, tr.x, tr.y);
-//	
-//    CGContextSaveGState(context);
-//    CGContextAddPath(context, cgPath);
-//    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-//    CGContextSetLineJoin(context, kCGLineJoinRound);
-//    CGContextSetLineCap(context, kCGLineCapRound);
-//    CGContextSetLineWidth(context, 2);
-//    CGContextStrokePath(context);
-//    CGPathRelease(cgPath);
-//    CGContextRestoreGState(context);
-	
-	/////////////
-
 	Portal *portal = (Portal *)self.overlay;
 
-    CGImageRef portalImage;
-	//CGImageRef resonatorImage = [UIImage imageNamed:@"resonatorFromTop.png"].CGImage;
+	if (portal.completeInfo) {
+		CGImageRef portalImage;
 
-	//[DeployedResonator MR_countOfEntitiesWithPredicate:[NSPredicate predicateWithFormat:@"portal = %@", portal]] 
-	if (portal.resonators.count > 0) {
-		if ([portal.controllingTeam isEqualToString:@"ALIENS"]) {
-			portalImage = [UIImage imageNamed:@"portal_aliens.png"].CGImage;
+		int count = [DeployedResonator MR_countOfEntitiesWithPredicate:[NSPredicate predicateWithFormat:@"portal = %@", portal]];
+		if (count > 0) {
+			if ([portal.controllingTeam isEqualToString:@"ALIENS"]) {
+				portalImage = [UIImage imageNamed:@"portal_aliens.png"].CGImage;
+			} else {
+				portalImage = [UIImage imageNamed:@"portal_resistance.png"].CGImage;
+			}
 		} else {
-			portalImage = [UIImage imageNamed:@"portal_resistance.png"].CGImage;
+			portalImage = [UIImage imageNamed:@"portal_neutral.png"].CGImage;
 		}
-	} else {
-		portalImage = [UIImage imageNamed:@"portal_neutral.png"].CGImage;
-	}
-	
-	MKMapPoint portalCenter = MKMapPointForCoordinate(self.overlay.coordinate);
-	CGPoint portalCenterPoint = [self pointForMapPoint:portalCenter];
-	
-	CGFloat portalSize = 400;
 
-    CGContextDrawImage(context, CGRectMake(portalCenterPoint.x-portalSize/2, portalCenterPoint.y-portalSize/2, portalSize, portalSize), portalImage);
+		MKMapPoint portalCenter = MKMapPointForCoordinate(self.overlay.coordinate);
+		CGPoint portalCenterPoint = [self pointForMapPoint:portalCenter];
+
+		CGFloat portalSize = 400;
+
+		CGContextDrawImage(context, CGRectMake(portalCenterPoint.x-portalSize/2, portalCenterPoint.y-portalSize/2, portalSize, portalSize), portalImage);
+	}
 
 }
 
